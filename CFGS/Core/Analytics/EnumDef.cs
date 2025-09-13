4d2cc12e8282d422cf58fe30be180bbef4fa6778
BlackFoX1991 <loewen.artur@gmail.com>
2025-09-13 09:36:29 +0200

    fix enum defintion.

--- CFGS/Core/Analytics/EnumDef.cs
+++ CFGS/Core/Analytics/EnumDef.cs
@@ -7,39 +7,29 @@ namespace CFGS.Core.Analytics;
 
 public class EnumDef
 {
-    public string Name { get; }
-    public Dictionary<string, int> Members { get; } = new();
+    public readonly string Name;
+    public readonly List<KeyValuePair<string, int?>> Members;
 
     public EnumDef(string name, List<KeyValuePair<string, int?>> members)
     {
         Name = name;
-        int counter = 0;
-        var usedValues = new HashSet<int>();
-
-        foreach (var kv in members)
-        {
-            int value = kv.Value ?? counter;
-
-            if (usedValues.Contains(value))
-                throw new Exception($"Duplicate enum value {value} in enum {name}.");
-
-            Members[kv.Key] = value;
-            usedValues.Add(value);
-
-            counter = value + 1;
-        }
+        Members = members;
     }
 
     public override string ToString()
     {
-        return JsonSerializer.Serialize(new
-        {
-            type = "Enum",
-            name = Name,
-            members = Members
-        });
+        var membersJson = Members
+            .Select(m => $"\"{m.Key}\": {m.Value?.ToString() ?? "null"}")
+            .Aggregate((current, next) => current + ", " + next);
+
+        return "{ " +
+               $"\"type\": \"Enum\", " +
+               $"\"name\": \"{Name}\", " +
+               $"\"members\": {{ {membersJson} }}" +
+               " }";
     }
 }
+
 
 
 
